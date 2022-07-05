00e94e6eb372428c0378f4c2f404abc2445ea413
noahcalibuso <31081555+noahcalibuso@users.noreply.github.com>
2022-07-05 15:15:53 -0700

    Improved Style and added task strikethrough

---
+++ Todo-List-main/lib/widgets/TaskItem.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/src/home.dart';
+
+class TaskItem extends StatelessWidget {
+  TaskItem({
+    required this.task,
+    required this.isChecked
+  }) : super(key: ObjectKey(task));
+
+
+  final Task task;
+  final isChecked;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+    return const TextStyle(
+      color: Colors.grey,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        isChecked(task);
+      },
+      leading: Icon(Icons.circle_rounded, color: Colors.blue[200]),
+      title: Text(task.name, style: _getTextStyle(task.checked)),
+    );
+  }
+}
